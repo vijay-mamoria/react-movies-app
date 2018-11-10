9eff43b94e927f073042f0b930b84dc9e9eed919
vijay-mamoria <theprofessionalcoder@gmail.com>
2018-11-11 00:36:44 +0530

    Added username and password inside Login form

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -1,10 +1,32 @@
+import { FormControl, InputLabel } from '@material-ui/core';
 import Button from '@material-ui/core/Button';
 import Tab from '@material-ui/core/Tab';
 import Tabs from '@material-ui/core/Tabs';
+import Typography from '@material-ui/core/Typography';
 import React, { Component } from 'react';
 import Modal from 'react-modal';
 import logo from '../../assets/logo.svg';
 import './Header.css';
+
+//For the modal to fit the content.
+const customStyles = {
+    content: {
+        top: '50%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        marginRight: '-50%',
+        transform: 'translate(-50%, -50%)'
+    }
+};
+
+const TabContainer = function (props) {
+    return (
+        <Typography component="div" style={{ padding: 0 }}>
+            {props.children}
+        </Typography>
+    );
+}
 
 class Header extends Component {
 
@@ -37,12 +59,23 @@ class Header extends Component {
                         <Button variant="contained" color="default" onClick={this.openModalHandler}>Login</Button>
                     </div>
                 </header>
-                <Modal ariaHideApp={false} contentLabel="Login" isOpen={this.state.isModalOpen} onRequestClose={this.closeModalHandler}>
+                <Modal ariaHideApp={false} contentLabel="Login" isOpen={this.state.isModalOpen}
+                    onRequestClose={this.closeModalHandler} style={customStyles}>
                     {/*The first tab has a position of 0, the second tab has a position of 1, and so on.*/}
                     <Tabs value={this.state.value} onChange={this.tabChangeHandler}>
                         <Tab label="Login"></Tab>
                         <Tab label="Register"></Tab>
                     </Tabs>
+                    <TabContainer>
+                        <FormControl required>
+                            <InputLabel htmlFor="username">Username</InputLabel>
+                            <input id="username" type="text"></input>
+                        </FormControl>
+                        <FormControl required>
+                            <InputLabel htmlFor="password">Password</InputLabel>
+                            <input id="password" type="password"></input>
+                        </FormControl>
+                    </TabContainer>
                 </Modal>
             </div>);
     }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'typeface-roboto';
 import './index.css';
-import * as serviceWorker from './serviceWorker';
 import Home from './screens/home/Home';
+import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Home/>, document.getElementById('root'));
+ReactDOM.render(<Home />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
